b44652ff2cb0aca478feb09519d84c2444e4d523
Damien Whitten <damien@rebase.com.au>
2016-05-09 17:41:39 +1000

    WIP Server

--- cmd/devn-server/devn-server.go
+++ cmd/devn-server/devn-server.go
@@ -1,10 +1,37 @@
 package main
 
+import (
+	"net/http"
+	"os"
+	"path"
+)
+
 /* devn-server
 a web api and websocket server
 * to monitor them. This might be uncomfortably large compared
 * to the other components.
 */
+
+var flags = struct {
+	builds string
+	bind   string
+}{}
+
+func HandleBuildInfo(rw http.ResponseWriter, req *http.Request) {
+	file, err := os.Open(path.Join(flags.builds, req))
+	if err != nil {
+		rw.WriteHeader(500)
+		fmt.Fprintf(rw, "Error: %s\n", err.Error())
+		return
+	}
+	defer file.Close()
+	io.Copy(rw, file)
+}
+
+func main() {
+	http.Handle("/builds/", http.StripPrefix("/builds", http.HandleFunc(HandleBuildInfo)))
+	http.ListenAndServe(bind, nil)
+}
 
 // Hook Endpoint
 
